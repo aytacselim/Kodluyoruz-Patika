5f07da6f3134120aeb0fa93cf4a11b182a362b47
aytacselim <100492865+aytacselim@users.noreply.github.com>
2022-08-08 21:22:02 +0300

    Gelen veriler tek bir objede birlestirildi.

--- React/Odev-1/index.js
+++ React/Odev-1/index.js
@@ -4,8 +4,8 @@ const getData = async id => {
     try {
         const user = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
         const posts = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
+        user.data.posts = posts.data
         console.log(user.data);
-        console.log(posts.data);
     } catch(e) {
         console.log(e);
     }
